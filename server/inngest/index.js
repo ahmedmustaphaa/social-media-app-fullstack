833ce6c10ea6a7b32ec6bc06d81c1f46837f8c3d
Ahmed Mustafa <ahmed@example.com>
2025-09-28 17:44:40 +0300

    new updated

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -1,4 +1,4 @@
-import { User } from "../model/User";
+import { User } from "../model/User.js";
 import { Inngest } from "inngest";
 export const inngest = new Inngest({ id: "movie-ticket-booking" });
 
